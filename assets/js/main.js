cfc975e375cceadbf391ef8c5f13d0b68df618e3
Aleksandr Volkov <volkov@1creator.ru>
2021-10-30 19:35:28 +0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-import Swiper from "swiper/swiper-bundle.js";
+import Swiper from 'swiper/swiper-bundle.js';
 
 let quizModal = document.getElementById('quiz-modal')
 
 
-quizModal.addEventListener('shown.bs.modal', function () {
+quizModal.addEventListener('shown.bs.modal', function() {
 
     const swiper = new Swiper('.swiper', {
         spaceBetween: 200,
@@ -15,13 +15,17 @@ quizModal.addEventListener('shown.bs.modal', function () {
             el: '.swiper-pagination',
             type: 'bullets',
         },
+        navigation: {
+            nextEl: '.quiz-btn-next',
+            prevEl: '.quiz-btn-prev',
+        }
     });
-    let btnNextArray = document.getElementsByClassName('quiz-btn-next')
-    let btnPrevArray = document.getElementsByClassName('quiz-btn-prev')
-    for (let item of btnNextArray) {
-        item.addEventListener('click', ()=> swiper.slideNext())
-    }
-    for (let item of btnPrevArray) {
-        item.addEventListener('click', ()=> swiper.slidePrev())
-    }
+    // let btnNextArray = document.getElementsByClassName('quiz-btn-next')
+    // let btnPrevArray = document.getElementsByClassName('quiz-btn-prev')
+    // for (let item of btnNextArray) {
+    //     item.addEventListener('click', ()=> swiper.slideNext())
+    // }
+    // for (let item of btnPrevArray) {
+    //     item.addEventListener('click', ()=> swiper.slidePrev())
+    // }
 })
