a8ceb1cbac4bacc5c741f7406107b1b18c0b1450
Volkov Aleksandr <Volkov@1creator.ru>
2021-10-31 00:14:14 +0500

    .

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+let mix = require('laravel-mix');
+const path = require('path')
+require('mix-html-builder');
+
+mix.setPublicPath('dist')
+
+mix.js('./src/assets/js/vendor.js', 'dist/js')
+    .js('./src/assets/js/main.js', 'dist/js')
+    .sass('./src/assets/sass/app.scss', '/dist/css')
+    .sass('./src/assets/sass/vendor.scss', '/dist/css')
+    .alias({
+        '@': path.join(__dirname, './src/assets')
+    });
+// .setResourceRoot('dist/assets')
+
+mix.html({
+    htmlRoot: './src/pages/**/*.html', // Your html root file(s)
+    output: '', // The html output folder
+    partialRoot: './src/partials',    // default partial path
+    layoutRoot: './src/layouts',    // default partial path
+    minify: {
+        removeComments: true
+    }
+});
