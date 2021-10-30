594833adb9b761b41425af47afa3e5699f95c63e
Aleksandr Volkov <volkov@1creator.ru>
2021-10-30 21:05:29 +0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,18 @@
-import Swiper from 'swiper/swiper-bundle.js';
+import { Swiper, Navigation, Pagination, } from 'swiper';
 
-let quizModal = document.getElementById('quiz-modal')
+Swiper.use([Navigation, Pagination])
 
-
-quizModal.addEventListener('shown.bs.modal', function() {
-
-    const swiper = new Swiper('.swiper', {
-        spaceBetween: 200,
-        allowTouchMove: false,
-        observer: true,
-        observeParents: true,
-        autoHeight: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-        },
-        navigation: {
-            nextEl: '.quiz-btn-next',
-            prevEl: '.quiz-btn-prev',
-        }
-    });
-    // let btnNextArray = document.getElementsByClassName('quiz-btn-next')
-    // let btnPrevArray = document.getElementsByClassName('quiz-btn-prev')
-    // for (let item of btnNextArray) {
-    //     item.addEventListener('click', ()=> swiper.slideNext())
-    // }
-    // for (let item of btnPrevArray) {
-    //     item.addEventListener('click', ()=> swiper.slidePrev())
-    // }
-})
+new Swiper('.quiz-modal__swiper', {
+    spaceBetween: 200,
+    allowTouchMove: false,
+    observer: true,
+    observeParents: true,
+    pagination: {
+        el: '.quiz-modal__pagination',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.quiz__next',
+        prevEl: '.quiz__prev',
+    }
+});
