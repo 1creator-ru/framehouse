fa9a0b31852722fb1c064bf3cb92060ae16a9f82
Volkov Aleksandr <Volkov@1creator.ru>
2021-10-31 18:17:08 +0500

    .

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,8 +6,7 @@ mix.setPublicPath('dist')
     .copyDirectory('./src/assets/images', './dist/assets/images')
     .copyDirectory('./src/assets/fonts', './dist/assets/fonts')
     .copyDirectory('./src/assets/sass/vendor/icons/fonts', './dist/assets/fonts/')
-    .js('./src/assets/js/vendor.js', './dist/assets/js')
-    .js('./src/assets/js/main.js', './dist/assets/js')
+    .js('./src/assets/js/app.js', './dist/assets/js')
     .sass('./src/assets/sass/app.scss', './dist/assets/css')
     .sass('./src/assets/sass/vendor.scss', './dist/assets/css')
     .options({
@@ -15,13 +14,3 @@ mix.setPublicPath('dist')
     })
 
 mix.handlebars('src/html', 'dist/');
-
-// mix.html({
-//     htmlRoot: './src/pages/**/*.html', // Your html root file(s)
-//     output: '', // The html output folder
-//     partialRoot: './src/partials',    // default partial path
-//     layoutRoot: './src/layouts',    // default partial path
-//     minify: {
-//         removeComments: true
-//     }
-// });
